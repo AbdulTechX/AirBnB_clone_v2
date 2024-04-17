73fb486c53a7b7f069db10480971e4fe56991f82
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-04-17 07:31:33 -0700

    do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,19 +6,18 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder
     """
-
     # Create timestamp for the archive name
-    timestamp = datetime now().strftime(’%Y%m%d%H%M%S’)
-    # Generate a .tgzfron the webstatic folder
-    archive_name = f’web_static_{timestamp}.tgz
+    timestamp = datetime.now().strftime(’%Y%m%d%H%M%S’)
+    # Generate a .tgz from the webstatic folder
+    archive_name = f'web_static_{timestamp}.tgz'
     archive_path = os.path.join ('versions',archive_name)
 
     if not os.path.exist ('versions'):
         os.mkdirs('versions')
 
     try:
-        local('tar -cvzf {} webstatic'.format(archieve-path))
-        return archive-path
+        local('tar -cvzf {} web_static'.format(archieve_path))
+        return archive_path
     except Exception as e:
         print("failed to generate archive:",e)
-           return none
+           return None
