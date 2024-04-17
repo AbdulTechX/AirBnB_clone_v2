9c5c439a37cc4663f4f6a39281d2231370b43c21
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-04-16 23:36:23 -0700

    web deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,20 +2,21 @@
 #Write a Bash script that sets up your web servers for the deployment of web_static
 
 # Install Nginx if not already install
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 
 # create a directory folder if doesnt already exist
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 
 # Create a fake HTML file
-echo "<html>
+sudo echo "<html>
 <head>
   <title>Test Page</title>
  </head>
@@ -23,36 +24,13 @@ echo "<html>
   <h1>Holberton School</h1>
   <p>Hello, world!</p>
  </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # create symbolic link /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://google.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /etc/nginx/html;
-      internal;
-    g
-
-}" > /etc/nginx/sites-enabled/default
-
-sudo service nginx restartt
+sudo service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,3 +2,15 @@ from fabric.api import local
 from date import datetime
 
 def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+
+    # Create the versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Generate the name of the archive
+    now = datetime.now()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second
+    )
