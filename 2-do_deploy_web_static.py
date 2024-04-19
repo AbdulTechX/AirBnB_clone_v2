3e1ea13e6db015db16aad08b103e1405c07659b2
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-04-19 13:01:25 -0700

    Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,42 +2,49 @@
 """fabric script that distribute an archive to web servers using
    using do_deploy function.
 """
-from fabric.api import env, put, run
-from os import path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+import os.path
 
-env.user = 'ubuntu'  # Set the SSH user dynamically
+env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 env.hosts = ['100.25.211.171', '100.24.72.44']
 
 
 def do_deploy(archive_path):
     """Deploys an archive to the web servers"""
-    if not (path.exists(archive_path)):
+    if os.path.isfile(archive_path) is False:
         return False
 
-    try:
-        # Upload archive to /tmp/ directory on servers
-        put(archive_path, '/tmp/')
+    # Extract archive to /data/web_static/releases/<archive_filename>
+    archive_file = archive_path.split('/')[-1]
+    archive_name = archive_file.split('.')[0]
 
-        # Extract archive to /data/web_static/releases/<archive_filename>
-        archive_filename = archive_path.split('/')[-1]
-        archive_no_ext = archive_filename.split('.')[0]
-
-        # Uncompress the archive to the folder 
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_no_ext))
-
-        # Delete the archive from tmp directory from the web server
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # delete symbolic link /data/web_static/current from the web server
-        run('rm -rf /data/web_static/current')
-
-
-        #Create a new the symbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_no_ext))
-
-        return True
-    except:
+    #Upload the archive 
+    if put(archive_path, "/tmp/{}".format(archive_file)).failed is True:
         return False
-
+    # Uncompress the archive to the folder
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(archive_name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(archive_name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(archive_file, archive_name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(archive_file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(archive_name, archive_name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(archive_name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(archive_name)).failed is True:
+        return False
+    return True
