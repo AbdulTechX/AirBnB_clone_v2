0e712c794637f8d3a466ae5230cce8ebabf1dbd5
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-04-20 20:34:15 -0700

    ful deployment

---
+++ 100-clean_web_static.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""write a fabric script that deletes out of date archive using
+   the function
+"""
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -16,19 +16,20 @@ env.hosts = ['100.25.211.171', '100.24.72.44']
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    """
-    try:
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-        archive_name = "versions/web_static_{}.tgz".format(timestamp)
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-        local("tar -cvzf {} web_static".format(archive_name))
-        return archive_name
-    except:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
 
 def do_deploy(archive_path):
     """Deploys an archive to the web servers"""
